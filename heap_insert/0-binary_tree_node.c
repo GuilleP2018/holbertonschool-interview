fecc7a5f8da44149ca1f77fabb6819516844ceb1
Guillep2018 <gpereyo@gmail.com>
2024-05-24 15:19:54 -0400

    compiled and fixed

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -13,12 +13,10 @@ binary_tree_t *new_node;
 
 new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
-return (NULL);
-
-new_node->n = value;
+    return (NULL);
 new_node->parent = parent;
+new_node->n = value;
 new_node->left = NULL;
 new_node->right = NULL;
-
 return (new_node);
 }
