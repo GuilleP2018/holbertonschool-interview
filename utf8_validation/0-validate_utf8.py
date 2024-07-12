7fed5ae85734ab3a198a6742127a371fa153d5fd
Guillep2018 <gpereyo@gmail.com>
2024-07-12 14:17:03 -0400

    fix

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """ validUTF8 """
 
+
 def validUTF8(data):
-    """ validUTF8
-    Returns true if data is a valid UTF-8 encoding, else returns false
-    """
-    if not data:
-        return False
-    for i in data:
-        if i < 0 or i > 255:
-            return False
-    return True
+    """Check if a list of integers represents a valid UTF-8 encoding."""
+    num_bytes = 0
+    for num in data:
+        binary_representation = format(num, '#010b')[-8:]
+        if num_bytes == 0:
+            for bit in binary_representation:
+                if bit == '0': break
+                num_bytes += 1
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not binary_representation.startswith('10'):
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
